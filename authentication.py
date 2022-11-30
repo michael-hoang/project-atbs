163db5f585a593cdcee2115f4a02f0cbfa23529c
michael-hoang <mikehoang31@gmail.com>
2022-11-30 01:19:01 -0800

    add pin verification, export encrypted token, refactor variable names

--- authentication.py
+++ authentication.py
@@ -1,12 +1,13 @@
 """This module contains a class that represents an authenticator."""
 
 import tkinter as tk
+from tkinter import messagebox
 import random
 
 from myEncryption import EnDeCrypt
 
 
-FONT = ('Bahnschrift Light', 14, 'normal')
+FONT = ('Bahnschrift Light', 12, 'normal')
 
 
 class Authenticator:
@@ -19,9 +20,10 @@ class Authenticator:
         self.root.title('Enter Pin')
 
         self.pin = ''
+        self.shiftNum = ''
         self.token = ''
         self.cred = self._check_for_existing_credential()
-        self._create_pin()
+        self._start()
 
         self.root.mainloop()
 
@@ -52,35 +54,71 @@ class Authenticator:
             return False
 
     def _start(self):
-        """Start authentication or prompt user to create pin."""
+        """Start authentication or prompt user to create secret key."""
         if self.cred:
             self._authenticate()
         else:
-            self._create_pin()
             self._generate_token()
+            self.shiftNum = str(random.choice(range(85)))
+            self._create_secretkey()
+
+    def _export_token(self):
+        """
+        Encrypt secret key and shift number, and embed inside token. Export
+        to file data.json and auth.key
+        """
+        if self.pin:
+            encryptedPin = self.endecrypt.encrypt(self.pin)
+            encryptedShiftNum = self.endecrypt.encrypt(self.shiftNum)
+            embeddedToken = self.token[:24] + encryptedPin + self.token[24:49] + encryptedShiftNum + self.token[49:]
+
+            print(encryptedPin)
+            print(encryptedShiftNum)
+            print(embeddedToken)
+            print(self.token)
+
 
     def _authenticate(self):
         """If credential exists, prompt user to enter pin."""
 
-    def _create_pin(self):
-        """Prompt user to create pin."""
-        warning_message = 'Create a 4-digit PIN.\n\nWARNING: Keep your PIN safe.\n'\
-            'Your stored accounts and passwords\ncannot be recovered if PIN  is lost.'
-        createPinWin = tk.Toplevel(self.root)
-        createPinWin.title('Create PIN')
+    def _create_secretkey(self):
+        """Prompt user to create a secret key."""
+
+        def _verifySecretKey():
+            """Verify if user re-enters secret key correctly."""
+            pin1 = keyEntry.get()
+            pin2 = verifyKeyEntry.get()
+            if pin1 == pin2:
+                self.pin = pin1
+                messagebox.showinfo(title='Success', message='Your PIN has been'
+                                    ' created. Don\'t lose it!')
+                self._export_token()
+                createKeyWin.destroy()
+            else:
+                messagebox.showerror(title='Error', message='Your PIN did not'
+                                     ' match. Please try again.')
+
+        warning_message = 'Create a secret PIN or phrase.\n\nWARNING: This will be'\
+        ' your secret key.\nKeep it safe. You will not be able'\
+        ' to recover\nyour saved accounts and passwords if you lose'\
+        ' it.'
+        createKeyWin = tk.Toplevel(self.root)
+        createKeyWin.title('Create PIN/Phrase')
         warningLabel = tk.Label(
-            createPinWin, text=warning_message, justify='left', font=FONT)
+            createKeyWin, text=warning_message, justify='left', font=FONT)
         warningLabel.grid(column=0, row=0, columnspan=2, sticky='EW')
-        enterPinLabel = tk.Label(createPinWin, text='Enter PIN: ', font=FONT)
-        enterPinLabel.grid(column=0, row=1, sticky='E')
-        pinEntry = tk.Entry(createPinWin, font=FONT)
-        pinEntry.grid(column=1, row=1)
-        verifyPinLabel = tk.Label(createPinWin, text='Re-Enter PIN: ', font=FONT)
-        verifyPinLabel.grid(column=0, row=2, sticky='E')
-        verifyPinEntry = tk.Entry(createPinWin, font=FONT)
-        verifyPinEntry.grid(column=1, row=2)
-        okButton = tk.Button(createPinWin, text='OK')
+        enterKeyLabel = tk.Label(createKeyWin, text='Enter PIN/Phrase: ', font=FONT)
+        enterKeyLabel.grid(column=0, row=1, sticky='E')
+        keyEntry = tk.Entry(createKeyWin, font=FONT)
+        keyEntry.grid(column=1, row=1)
+        verifyKeyLabel = tk.Label(
+            createKeyWin, text='Re-Enter PIN/Phrase: ', font=FONT)
+        verifyKeyLabel.grid(column=0, row=2, sticky='E')
+        verifyKeyEntry = tk.Entry(createKeyWin, font=FONT)
+        verifyKeyEntry.grid(column=1, row=2)
+        okButton = tk.Button(createKeyWin, text='OK', command=_verifySecretKey)
         okButton.grid(column=1, row=3)
+
 
     def _generate_token(self, size=100):
         """Generate a random token of default size 100."""
@@ -95,6 +133,4 @@ class Authenticator:
 
 if __name__ == '__main__':
     a = Authenticator()
-    a._generate_token()
-    # print(a.cred)
-    # print(a.token)
+ 
