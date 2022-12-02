0ebc71299aee16b87f9092d031a9af30431ab911
michael-hoang <mikehoang31@gmail.com>
2022-12-01 21:59:18 -0800

    create 2 functions: _encrypt_token and _export_token

--- authentication.py
+++ authentication.py
@@ -3,6 +3,8 @@
 import tkinter as tk
 from tkinter import messagebox
 import random
+import json
+import os
 
 from myEncryption import EnDeCrypt
 
@@ -19,7 +21,7 @@ class Authenticator:
         self.root = tk.Tk()
         self.root.title('Enter Pin')
 
-        self.pin = ''
+        self.secretKey = ''
         self.shiftNum = ''
         self.token = ''
         self.cred = self._check_for_existing_credential()
@@ -44,6 +46,7 @@ class Authenticator:
         if cred1 and cred2:
             return True
         elif cred1 and not cred2:
+            # ask if user wants to overwrite existing file and create new credential.
             print("'cred/auth.key' verified.\n'data/data.json' missing.")
             return
         elif not cred1 and cred2:
@@ -62,52 +65,74 @@ class Authenticator:
             self.shiftNum = str(random.choice(range(85)))
             self._create_secretkey()
 
-    def _export_token(self):
-        """
-        Encrypt secret key and shift number, and embed inside token. Export
-        to file data.json and auth.key
-        """
-        if self.pin:
-            encryptedPin = self.endecrypt.encrypt(self.pin)
-            encryptedShiftNum = self.endecrypt.encrypt(self.shiftNum)
-            embeddedToken = self.token[:24] + encryptedPin + self.token[24:49] + encryptedShiftNum + self.token[49:]
+    def _encrypt_token(self) -> str:
+        """Encrypt and embed secret key and shift number inside token. Return encrypted token."""
+        encryptedKey = self.endecrypt.encrypt(self.secretKey)
+        encryptedShiftNum = self.endecrypt.encrypt(self.shiftNum)
+        embeddedToken = self.token[:24] + encryptedKey + \
+            self.token[24:49] + encryptedShiftNum + self.token[49:]
+        encryptedToken = self.endecrypt.encrypt(embeddedToken)
+        return encryptedToken
 
-            print(encryptedPin)
-            print(encryptedShiftNum)
-            print(embeddedToken)
-            print(self.token)
+    def _export_token(self, token: str):
+        """Separate encrypted token into two parts and write each part to data.json and auth.key."""
+        tokenPiece1 = token[:int(len(token)/2)]
+        tokenPiece2 = token[int(len(token)/2):]
+        relPath1 = 'data'
+        relPath2 = 'cred'
+        mainPath = os.path.dirname(__file__)
+        absolutePath1 = os.path.join(mainPath, relPath1)
+        absolutePath2 = os.path.join(mainPath, relPath2)
+        isExist1 = os.path.exists(absolutePath1)
+        isExist2 = os.path.exists(absolutePath2)
 
+        if not isExist1:
+            os.mkdir(absolutePath1)
+
+        if not isExist2:
+            os.mkdir(absolutePath2)
+
+        with open('data/data.json', 'w') as f:
+            data = {'key': tokenPiece1}
+            json.dump(data, f, indent=4)
+
+        with open('cred/auth.key', 'w') as f:
+            f.write(tokenPiece2)
+
+        print(token)
 
     def _authenticate(self):
-        """If credential exists, prompt user to enter pin."""
+        """If credential exists, prompt user to enter secret key."""
 
     def _create_secretkey(self):
         """Prompt user to create a secret key."""
 
         def _verifySecretKey():
             """Verify if user re-enters secret key correctly."""
-            pin1 = keyEntry.get()
-            pin2 = verifyKeyEntry.get()
-            if pin1 == pin2:
-                self.pin = pin1
-                messagebox.showinfo(title='Success', message='Your PIN has been'
+            key1 = keyEntry.get()
+            key2 = verifyKeyEntry.get()
+            if key1 == key2:
+                self.secretKey = key1
+                messagebox.showinfo(title='Success', message='Your secret key has been'
                                     ' created. Don\'t lose it!')
-                self._export_token()
+                encryptedToken = self._encrypt_token()
+                self._export_token(token=encryptedToken)
                 createKeyWin.destroy()
             else:
-                messagebox.showerror(title='Error', message='Your PIN did not'
+                messagebox.showerror(title='Error', message='Your secret key did not'
                                      ' match. Please try again.')
 
         warning_message = 'Create a secret PIN or phrase.\n\nWARNING: This will be'\
-        ' your secret key.\nKeep it safe. You will not be able'\
-        ' to recover\nyour saved accounts and passwords if you lose'\
-        ' it.'
+            ' your secret key.\nKeep it safe. You will not be able'\
+            ' to recover\nyour saved accounts and passwords if you lose'\
+            ' it.'
         createKeyWin = tk.Toplevel(self.root)
         createKeyWin.title('Create PIN/Phrase')
         warningLabel = tk.Label(
             createKeyWin, text=warning_message, justify='left', font=FONT)
         warningLabel.grid(column=0, row=0, columnspan=2, sticky='EW')
-        enterKeyLabel = tk.Label(createKeyWin, text='Enter PIN/Phrase: ', font=FONT)
+        enterKeyLabel = tk.Label(
+            createKeyWin, text='Enter PIN/Phrase: ', font=FONT)
         enterKeyLabel.grid(column=0, row=1, sticky='E')
         keyEntry = tk.Entry(createKeyWin, font=FONT)
         keyEntry.grid(column=1, row=1)
@@ -118,7 +143,6 @@ class Authenticator:
         verifyKeyEntry.grid(column=1, row=2)
         okButton = tk.Button(createKeyWin, text='OK', command=_verifySecretKey)
         okButton.grid(column=1, row=3)
-
 
     def _generate_token(self, size=100):
         """Generate a random token of default size 100."""
@@ -133,4 +157,3 @@ class Authenticator:
 
 if __name__ == '__main__':
     a = Authenticator()
- 
