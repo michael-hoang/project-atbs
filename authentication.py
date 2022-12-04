fccff5dbc5c8b6e95e42203f3570efd7ab093870
michael-hoang <mikehoang31@gmail.com>
2022-12-03 19:50:10 -0800

    move methods around for organization

--- authentication.py
+++ authentication.py
@@ -5,8 +5,6 @@ from tkinter import messagebox
 import random
 import json
 import os
-from pwManager import PasswordManager
-
 from myEncryption import EnDeCrypt
 
 
@@ -41,6 +39,16 @@ class Authenticator:
         self.cred = ''
         self._authenticate()
 
+    def _authenticate(self):
+        """Start authentication or prompt user to create secret key."""
+        self.cred = self._check_for_existing_credential()
+        if self.cred:
+            self.top.deiconify()
+        else:
+            self._generate_token()
+            self.shiftNum = str(random.choice(range(85)))
+            self._create_secretkey()
+
     def _check_for_existing_credential(self) -> bool:
         """Check for existing crendential (data.json and auth.key)."""
         try:
@@ -68,94 +76,15 @@ class Authenticator:
             print("'cred/auth.key' missing.\n'data/data.json' missing.")
             return False
 
-    def _authenticate(self):
-        """Start authentication or prompt user to create secret key."""
-        self.cred = self._check_for_existing_credential()
-        if self.cred:
-            self.top.deiconify()
+    def _generate_token(self, size=150):
+        """Generate a random token of default size 100."""
+        characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&\'()*+,-./:;<=>?@"[]^_`{|}~'
+        token = ''
+        for _ in range(size):
+            randChar = random.choice(characters)
+            token += randChar
 
-        else:
-            self._generate_token()
-            self.shiftNum = str(random.choice(range(85)))
-            self._create_secretkey()
-
-    def _encrypt_token(self) -> str:
-        """Encrypt and embed secret key inside token. Attach unencrypted shift number at the end of encrypted token."""
-        encryptedKey = self.endecrypt.encrypt(
-            message=self.secretKey, shiftNum=self.shiftNum)
-        embeddedToken = self.token[:75] + encryptedKey + self.token[75:]
-        encryptedKeyLen = len(encryptedKey)
-        encryptedToken = str(encryptedKeyLen) + "::" + self.endecrypt.encrypt(
-            message=embeddedToken, shiftNum=self.shiftNum) + "::" + self.shiftNum
-        return encryptedToken
-
-    def _export_token(self, token: str):
-        """Separate encrypted token into two parts and write each part to data.json and auth.key."""
-        tokenPiece1 = token[:int(len(token)/2)]
-        tokenPiece2 = token[int(len(token)/2):]
-        relPath1 = 'data'
-        relPath2 = 'cred'
-        mainPath = os.path.dirname(__file__)
-        absolutePath1 = os.path.join(mainPath, relPath1)
-        absolutePath2 = os.path.join(mainPath, relPath2)
-        isExist1 = os.path.exists(absolutePath1)
-        isExist2 = os.path.exists(absolutePath2)
-
-        if not isExist1:
-            os.mkdir(absolutePath1)
-
-        if not isExist2:
-            os.mkdir(absolutePath2)
-
-        with open('data/data.json', 'w') as f:
-            data = {'key': tokenPiece1}
-            json.dump(data, f, indent=4)
-
-        with open('cred/auth.key', 'w') as f:
-            f.write(tokenPiece2)
-
-    def _get_token(self) -> str:
-        """Get token from data.json and auth.key"""
-        with open('data/data.json', 'r') as f:
-            data = json.load(f)
-            tokenPiece1 = data['key']
-
-        with open('cred/auth.key', 'r') as f:
-            tokenPiece2 = f.read()
-
-        token = tokenPiece1 + tokenPiece2
-        return token
-
-    def _decrypt_token(self) -> str:
-        """Decrypt token to get secret key."""
-        token = self._get_token()
-        splitToken = token.split('::')
-        shiftNum = splitToken[-1]
-        encryptedKeyLength = int(splitToken[0])
-        bareToken = splitToken[1]
-        decryptedToken = self.endecrypt.decrypt(
-            encryptedMessage=bareToken, shiftNum=shiftNum)
-        encryptedKey = decryptedToken[75:76+encryptedKeyLength]
-        decryptedKey = self.endecrypt.decrypt(
-            encryptedMessage=encryptedKey, shiftNum=shiftNum)
-        return decryptedKey
-
-    def _verify_entered_key(self, stored_key) -> bool:
-        """If entered key matches the stored key inside the token, open Password Manager."""
-        entered_key = self.keyEntry.get()
-        if entered_key == stored_key:
-            self.isVerified = True
-            print(self.isVerified)
-
-        else:
-            messagebox.showerror(
-                title='Error', message='Invalid PIN or phrase.')
-            print(stored_key)
-
-    def confirmation(self):
-        """Confirm if user can access Password Manager through authentication."""
-        decryptedKey = self._decrypt_token()
-        self._verify_entered_key(stored_key=decryptedKey)
+        self.token = token
 
     def _create_secretkey(self):
         """Prompt user to create a secret key."""
@@ -200,15 +129,83 @@ class Authenticator:
 
         createKeyWin.protocol('WM_DELETE_WINDOW', self.top.destroy)
 
-    def _generate_token(self, size=150):
-        """Generate a random token of default size 100."""
-        characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&\'()*+,-./:;<=>?@"[]^_`{|}~'
-        token = ''
-        for _ in range(size):
-            randChar = random.choice(characters)
-            token += randChar
+    def _encrypt_token(self) -> str:
+        """Encrypt and embed secret key inside token. Attach unencrypted shift number at the end of encrypted token."""
+        encryptedKey = self.endecrypt.encrypt(
+            message=self.secretKey, shiftNum=self.shiftNum)
+        embeddedToken = self.token[:75] + encryptedKey + self.token[75:]
+        encryptedKeyLen = len(encryptedKey)
+        encryptedToken = str(encryptedKeyLen) + "::" + self.endecrypt.encrypt(
+            message=embeddedToken, shiftNum=self.shiftNum) + "::" + self.shiftNum
+        return encryptedToken
 
-        self.token = token
+    def _export_token(self, token: str):
+        """Separate encrypted token into two parts and write each part to data.json and auth.key."""
+        tokenPiece1 = token[:int(len(token)/2)]
+        tokenPiece2 = token[int(len(token)/2):]
+        relPath1 = 'data'
+        relPath2 = 'cred'
+        mainPath = os.path.dirname(__file__)
+        absolutePath1 = os.path.join(mainPath, relPath1)
+        absolutePath2 = os.path.join(mainPath, relPath2)
+        isExist1 = os.path.exists(absolutePath1)
+        isExist2 = os.path.exists(absolutePath2)
+
+        if not isExist1:
+            os.mkdir(absolutePath1)
+
+        if not isExist2:
+            os.mkdir(absolutePath2)
+
+        with open('data/data.json', 'w') as f:
+            data = {'key': tokenPiece1}
+            json.dump(data, f, indent=4)
+
+        with open('cred/auth.key', 'w') as f:
+            f.write(tokenPiece2)
+
+    def confirmation(self):
+        """Confirm if user can access Password Manager through authentication."""
+        decryptedKey = self._decrypt_token()
+        self._verify_entered_key(stored_key=decryptedKey)
+
+    def _decrypt_token(self) -> str:
+        """Decrypt token to get secret key."""
+        token = self._get_token()
+        splitToken = token.split('::')
+        shiftNum = splitToken[-1]
+        encryptedKeyLength = int(splitToken[0])
+        bareToken = splitToken[1]
+        decryptedToken = self.endecrypt.decrypt(
+            encryptedMessage=bareToken, shiftNum=shiftNum)
+        encryptedKey = decryptedToken[75:76+encryptedKeyLength]
+        decryptedKey = self.endecrypt.decrypt(
+            encryptedMessage=encryptedKey, shiftNum=shiftNum)
+        return decryptedKey
+
+    def _get_token(self) -> str:
+        """Get token from data.json and auth.key"""
+        with open('data/data.json', 'r') as f:
+            data = json.load(f)
+            tokenPiece1 = data['key']
+
+        with open('cred/auth.key', 'r') as f:
+            tokenPiece2 = f.read()
+
+        token = tokenPiece1 + tokenPiece2
+        return token
+
+    def _verify_entered_key(self, stored_key) -> bool:
+        """If entered key matches the stored key inside the token, open Password Manager."""
+        entered_key = self.keyEntry.get()
+        if entered_key == stored_key:
+            self.isVerified = True
+            print(self.isVerified)
+
+        else:
+            messagebox.showerror(
+                title='Error', message='Invalid PIN or phrase.')
+            print(stored_key)
 
 
 if __name__ == '__main__':
