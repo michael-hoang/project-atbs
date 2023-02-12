4714b86224d5886c0354c4f24d48fb6cc3ba8b7b
michael-hoang <mike081522@gmail.com>
2023-02-11 23:58:40 -0800

    Create RefillTemplate class

--- refill.py
+++ refill.py
@@ -1,1 +1,12 @@
 """ This module contains a class that represents Refill Coordination form."""
+
+import tkinter as tk
+from tkinter import messagebox, END
+from PIL import Image, ImageTk
+
+
+class RefillTemplate:
+    """This class represents a GUI template that handles refill questions and formatting."""
+
+    def __init__(self):
+        """Initialize template window and refill questions."""
