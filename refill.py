14d76c5c36dff3baa6589650eb598e91f5be2e50
michael-hoang <mike081522@gmail.com>
2023-03-22 01:31:27 -0700

    Create check button canvas

--- refill.py
+++ refill.py
@@ -15,11 +15,40 @@ class RefillTemplate:
         self.top = tk.Toplevel()
         self.top.title('Refill Coordination')
         
-        # Template
+        # === RF template GUI === #
         self.medication_label = tk.Label(self.top, text='Medication:')
         self.medication_label.grid(column=0, row=0)
         self.medication_entry = tk.Entry(self.top)
         self.medication_entry.grid(column=1, row=0)
+
+        self.hipaa_verification_label = tk.Label(self.top, text='Methods of HIPAA Verification:')
+        self.hipaa_verification_label.grid(column=0, row=1)
+        # === Check button canvas === #
+        self.hipaa_checkbtn_canvas = tk.Canvas(self.top)
+        self.hipaa_checkbtn_canvas.grid(column=1, row=1)
+        # Name
+        self.hipaa_name_checkbtn = tk.Checkbutton(self.hipaa_checkbtn_canvas)
+        self.hipaa_name_checkbtn.grid(column=0, row=0)
+        self.hippa_name_label = tk.Label(self.hipaa_checkbtn_canvas, text='Name')
+        self.hippa_name_label.grid(column=1, row=0)
+        # DOB
+        self.hipaa_dob_checkbtn = tk.Checkbutton(self.hipaa_checkbtn_canvas)
+        self.hipaa_dob_checkbtn.grid(column=2, row=0)
+        self.hippa_dob_label = tk.Label(self.hipaa_checkbtn_canvas, text='DOB')
+        self.hippa_dob_label.grid(column=3, row=0)
+        # Address
+        self.hipaa_address_checkbtn = tk.Checkbutton(self.hipaa_checkbtn_canvas)
+        self.hipaa_address_checkbtn.grid(column=4, row=0)
+        self.hippa_address_label = tk.Label(self.hipaa_checkbtn_canvas, text='Address')
+        self.hippa_address_label.grid(column=5, row=0)
+        # Drug prescribed
+        self.hipaa_drug_checkbtn = tk.Checkbutton(self.hipaa_checkbtn_canvas)
+        self.hipaa_drug_checkbtn.grid(column=6, row=0)
+        self.hippa_drug_label = tk.Label(self.hipaa_checkbtn_canvas, text='Drug Prescribed')
+        self.hippa_drug_label.grid(column=7, row=0)
+        
+        self.changes = tk.Label(self.top, text='Changes Since Last Visit:')
+        self.changes.grid(column=0, row=2)
 
         
 
