4004ae7bdda1bc4b5f340b30f5d029679634385f
michael-hoang <mike081522@gmail.com>
2023-03-22 21:55:38 -0700

    Add label frame

--- refill.py
+++ refill.py
@@ -14,18 +14,20 @@ class RefillTemplate:
 
         self.top = tk.Toplevel()
         self.top.title('Refill Coordination')
+        self.top.config(padx=20, pady=20)
         
-        # === RF template GUI === #
-        self.medication_label = tk.Label(self.top, text='Medication:')
-        self.medication_label.grid(column=0, row=0)
-        self.medication_entry = tk.Entry(self.top)
-        self.medication_entry.grid(column=1, row=0)
+        # === Medication label frame === #
+        self.medication_labelFrame = tk.LabelFrame(self.top, text='Medication')
+        self.medication_labelFrame.grid(column=0, row=0, sticky='w')
+        self.medication_entry = tk.Entry(self.medication_labelFrame)
+        self.medication_entry.grid(column=1, row=0, sticky='w')
 
-        self.hipaa_verification_label = tk.Label(self.top, text='Methods of HIPAA Verification:')
-        self.hipaa_verification_label.grid(column=0, row=1)
-        # === Check button canvas === #
-        self.hipaa_checkbtn_canvas = tk.Canvas(self.top)
-        self.hipaa_checkbtn_canvas.grid(column=1, row=1)
+        # === HIPPA label frame === #
+        self.hipaa_labelFrame = tk.LabelFrame(self.top, text='Methods of HIPAA Verfication')
+        self.hipaa_labelFrame.grid(column=0, row=1, sticky='w')
+        #   Check button canvas
+        self.hipaa_checkbtn_canvas = tk.Canvas(self.hipaa_labelFrame)
+        self.hipaa_checkbtn_canvas.grid(column=1, row=0, sticky='w')
         # Name
         self.hipaa_name_checkbtn = tk.Checkbutton(self.hipaa_checkbtn_canvas)
         self.hipaa_name_checkbtn.grid(column=0, row=0)
@@ -47,8 +49,40 @@ class RefillTemplate:
         self.hippa_drug_label = tk.Label(self.hipaa_checkbtn_canvas, text='Drug Prescribed')
         self.hippa_drug_label.grid(column=7, row=0)
         
-        self.changes = tk.Label(self.top, text='Changes Since Last Visit:')
-        self.changes.grid(column=0, row=2)
+        # === Changes label frame === #
+        self.changes_labelFrame = tk.LabelFrame(self.top, text='Changes Since Last Visit')
+        self.changes_labelFrame.grid(column=0, row=2, sticky='w')
+        #   Check button canvas
+        self.changes_checkbtn_canvas = tk.Canvas(self.changes_labelFrame)
+        self.changes_checkbtn_canvas.grid(column=1, row=2, sticky='w')
+        # None
+        self.changes_none_checkbtn = tk.Checkbutton(self.changes_checkbtn_canvas)
+        self.changes_none_checkbtn.grid(column=0, row=0, sticky='w')
+        self.changes_none_label = tk.Label(self.changes_checkbtn_canvas, text='None')
+        self.changes_none_label.grid(column=1, row=0, sticky='w', columnspan=4)
+        # Changes to dose/direction
+        self.changes_dose_checkbtn = tk.Checkbutton(self.changes_checkbtn_canvas)
+        self.changes_dose_checkbtn.grid(column=0, row=1, sticky='w')
+        self.changes_dose_label = tk.Label(self.changes_checkbtn_canvas, text='Changes to dose/directions')
+        self.changes_dose_label.grid(column=1, row=1, sticky='w', columnspan=4)
+        # Changes to medication profile
+        self.changes_medProfile_checkbtn = tk.Checkbutton(self.changes_checkbtn_canvas)
+        self.changes_medProfile_checkbtn.grid(column=0, row=2, sticky='w')
+        self.changes_medProfile_label = tk.Label(self.changes_checkbtn_canvas, text='Changes to medication profile')
+        self.changes_medProfile_label.grid(column=1, row=2, sticky='w', columnspan=4)
+        # Other
+        self.changes_other_checkbtn = tk.Checkbutton(self.changes_checkbtn_canvas)
+        self.changes_other_checkbtn.grid(column=0, row=3, sticky='w')
+        self.changes_other_label = tk.Label(self.changes_checkbtn_canvas, text='Other:')
+        self.changes_other_label.grid(column=1, row=3, sticky='w')
+        self.changes_other_entry = tk.Entry(self.changes_checkbtn_canvas)
+        self.changes_other_entry.grid(column=2, row=3, columnspan=4, sticky='w')
+
+        # === Medication on hand label frame === #
+        self.medication_on_hand_labelFrame = tk.LabelFrame(self.top, text='Medication On Hand')
+        self.medication_on_hand_labelFrame.grid(column=0, row=3)
+        
+
 
         
 
