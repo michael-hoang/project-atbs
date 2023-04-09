41432725fc9267406a80c5eeee1eaa0e57a153d5
michael-hoang <mike081522@gmail.com>
2023-04-09 01:10:58 -0700

    Create entry widgets with labels

--- cardpayment2.py
+++ cardpayment2.py
@@ -31,20 +31,75 @@ class CardPayment(tkb.Frame):
         self.price_5 = tkb.StringVar(value='')
 
         # Form entries
-        self.create_form_entry('Credit Card No.', self.card_no)
-        self.create_form_entry('Exp:', self.exp)
-        self.create_form_entry('Security No.', self.security_no)
+        self.create_card_info_entries().pack(side=BOTTOM, fill=X, expand=YES, pady=5, padx=(18,0))
+        self.create_entries_column().pack(side=TOP, fill=X, expand=YES, pady=5)
+   
+    def create_long_form_entry(self, master, label, variable):
+        """Create a single long form entry."""
+        container = tkb.Frame(master)
+        lbl = tkb.Label(container, text=label, width=25)
+        lbl.pack(side=TOP, anchor='w')
+        ent = tkb.Entry(container, textvariable=variable, width=25)
+        ent.pack(side=LEFT, fill=X, expand=YES)
+        return container
 
-    def create_form_entry(self, label, variable):
-        """Create a single form entry."""
+    def create_short_form_entry(self, master, label, variable):
+        """Create a single short form entry."""
+        container = tkb.Frame(master)
+        lbl = tkb.Label(container, text=label, width=13)
+        lbl.pack(side=TOP, anchor='w')
+        ent = tkb.Entry(container, textvariable=variable, width=8)
+        ent.pack(side=LEFT, fill=X, expand=YES)
+        return container
+
+    def create_card_info_entries(self):
+        """Create card payment information entries."""
         container = tkb.Frame(self)
-        container.pack(fill=X, expand=YES, pady=5)
+        grid_para = {'pady': (4,0), 'sticky': 'w'}
+        cardnumber = self.create_long_form_entry(container, 'Card Number', self.card_no)
+        cardnumber.grid(column=0, row=0, columnspan=2, sticky='w')
+        exp = self.create_short_form_entry(container, 'Exp', self.exp)
+        exp.grid(column=0, row=1, **grid_para)
+        security = self.create_short_form_entry(container, 'Security Code', self.security_no)
+        security.grid(column=1, row=1, **grid_para)
+        cardholder = self.create_long_form_entry(container, 'Cardholder Name', self.cardholder)
+        cardholder.grid(column=0, row=2, columnspan=2, **grid_para)
+        mrn = self.create_long_form_entry(container, 'MRN', self.mrn)
+        mrn.grid(column=0, row=3, columnspan=2, **grid_para)
+        return container
 
-        lbl = tkb.Label(container, text=label, width=14, anchor='e')
-        lbl.pack(side=LEFT, padx=5)
+    def create_list_entry(self, master, item_label, item_var, price_var):
+        """Create a single item form entry."""
+        container = tkb.Frame(master)
+        container.pack(fill=X, expand=YES)
+        item_lbl = tkb.Label(container, text=item_label, width=2, anchor='e')
+        item_lbl.pack(side=LEFT, padx=(0,3))
+        item_ent = tkb.Entry(container, textvariable=item_var, width=25)
+        item_ent.pack(side=LEFT, fill=X, expand=YES)
+        price_lbl = tkb.Label(container, text='$', width=2, anchor='e')
+        price_lbl.pack(side=LEFT, padx=(0,3))
+        price_ent = tkb.Entry(container, textvariable=price_var, width=10)
+        price_ent.pack(side=LEFT, fill=X, expand=YES)
 
-        entry = tkb.Entry(container, textvariable=variable)
-        entry.pack(side=LEFT, padx=5, fill=X, expand=YES)
+    def create_entries_column(self):
+        """Create a list column of 5 entries."""
+        container = tkb.Frame(self)
+        self.create_list_header(container)
+        self.create_list_entry(container, '1.', self.med_1, self.price_1)
+        self.create_list_entry(container, '2.', self.med_2, self.price_2)
+        self.create_list_entry(container, '3.', self.med_3, self.price_3)
+        self.create_list_entry(container, '4.', self.med_4, self.price_4)
+        self.create_list_entry(container, '5.', self.med_5, self.price_5)
+        return container
+    
+    def create_list_header(self, master):
+        """Create labels for the list header."""
+        container = tkb.Frame(master)
+        container.pack(fill=X, expand=YES)
+        item_col_lbl = tkb.Label(container, text='Medication')
+        item_col_lbl.pack(side=LEFT, padx=(70,0))
+        price_col_lbl = tkb.Label(container, text='Price')
+        price_col_lbl.pack(side=RIGHT, padx=(0,25))
 
 
 if __name__ == '__main__':
