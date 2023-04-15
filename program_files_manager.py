0d923212082b4929b2403072679bafdc2fa91cdb
michael-hoang <mike081522@gmail.com>
2023-04-14 21:53:08 -0700

    Create class ProgramFilesManager

--- program_files_manager.py
+++ program_files_manager.py
@@ -1,2 +1,14 @@
 """This module provides a class that handles critical files necessary for the Main app to operate correctly."""
 
+from assetmanager import AssetManager
+
+
+class ProgramFilesManager:
+    """
+    This class provides methods to retrieve the paths to the running script or
+    executable and download all critical files required for the Main app to
+    function correctly.
+    """
+
+    def __init__(self):
+        pass
