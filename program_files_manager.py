63707c5159767267da843c2ee5520299b3984a99
michael-hoang <mike081522@gmail.com>
2023-04-14 23:00:46 -0700

    Create get_program_path and create_required_directories methods

--- program_files_manager.py
+++ program_files_manager.py
@@ -31,19 +31,42 @@ class ProgramFileManager:
         except:
             return None
 
-    def get_program_path(self) -> str:
+    def get_program_path(self):
         """
-        Return the absolute path to the running program. The program can be an
-        executable or a script.
+        Create an attribute for the absolute path to the running program (root 
+        path). The program can be an executable or a script.
         """
         if getattr(sys, 'frozen', False):
             path = os.path.abspath(sys.executable)
         else:
             path = os.path.abspath(__file__)
-        return path
-    
-    
+        self.root_path = path
 
+    def create_required_directories(self):
+        """
+        Create all the necessary directories that contain essential files for
+        the Main app.
+        """
+        assets_dir = os.path.join(self.root_path, 'assets')
+        form_dir = os.path.join(assets_dir, 'form')
+        img_dir = os.path.join(assets_dir, 'img')
+        dist_dir = os.path.join(self.root_path, 'dist')
+        current_version_dir = os.path.join(dist_dir, 'current_version')
+        directories = (
+            assets_dir, form_dir, img_dir, dist_dir, current_version_dir
+        )
+        for directory in directories:
+            if not os.path.exists(directory):
+                os.mkdir(directory)
+
+    def download_essential_files(self):
+        """
+        Download all essential files required for the Main app to run properly.
+        Instantiates an object called AssetManager to look for required files.
+        """
+        am = AssetManager()
+        self.get_program_path()
+        self.create_required_directories()
 
 
 if __name__ == '__main__':
