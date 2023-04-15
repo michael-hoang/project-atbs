7de69a181ee21d1745e3bae93d90807306dc3b4f
michael-hoang <mike081522@gmail.com>
2023-04-14 22:32:37 -0700

    Create get_latest_version_number method

--- program_files_manager.py
+++ program_files_manager.py
@@ -1,5 +1,8 @@
 """This module provides a class that handles critical files necessary for the Main app to operate correctly."""
 
+import json
+import requests
+
 from assetmanager import AssetManager
 
 
@@ -15,3 +18,14 @@ class ProgramFileManager:
 
         self.latest_version_url = 'https://raw.githubusercontent.com/michael-hoang/project-atbs-work/main/dist/latest_version/latest_main_version.json'
     
+
+    def get_latest_version_number(self) -> str:
+        """Return the latest version number for the Main app."""
+        try:
+            response = requests.get(self.latest_version_url)
+            if response.status_code == 200:
+                data = json.loads(response.content)
+                latest_version = data['main']
+                return latest_version
+        except:
+            return None
