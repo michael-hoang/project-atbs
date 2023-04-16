957944709e56053d0b72dc159129ffd7b6cdcce6
michael-hoang <mike081522@gmail.com>
2023-04-16 01:01:39 -0700

    Create RefillTemplate class that inherits from ttkinterbootstrap Frame

--- refill-modern.py
+++ refill-modern.py
@@ -11,3 +11,18 @@ import ttkbootstrap as tkb
 from win32 import win32clipboard
 from tkinter import WORD
 
+
+class RefillTemplate(tkb.Frame):
+    """This class represents a GUI template that handles refill qeustions and formatting."""
+
+    def __init__(self,master):
+        super().__init__(master, padding=(20, 10))
+
+
+if __name__ == '__main__':
+    app = tkb.Window(
+        'Refill Coordination', 'superhero', resizable=(False, False)
+    )
+    RefillTemplate(app)
+    app.place_window_center()
+    app.mainloop()
