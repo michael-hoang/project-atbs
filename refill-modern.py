decb63736a6d1917714a61497577ff220a0e0388
michael-hoang <mike081522@gmail.com>
2023-04-16 00:43:34 -0700

    Create refill-modern.py, import required modules

---
+++ refill-modern.py
@@ -0,0 +1,13 @@
+"""This module contains a class that represents Refill Coordination form."""
+
+import datetime as dt
+import json
+import os
+import sys
+import subprocess
+import tkinter as tk
+import ttkbootstrap as tkb
+
+from win32 import win32clipboard
+from tkinter import WORD
+
