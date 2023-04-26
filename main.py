c9e23fe4f4a56ca6df8757fe2515a4edc0a5ba8e
michael-hoang <mike081522@gmail.com>
2023-04-26 00:19:43 -0700

    Remove deprecated img assets

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import tkinter as tk
 import ttkbootstrap as tkb
 from tkinter import messagebox
-from PIL import Image, ImageTk
-from wrapup import WrapUpDateCalculator
 from cardpayment import CardPayment
-from map import Map
-from pwmanager import PasswordManager
-from authentication import Authenticator
-from refill import RefillTemplate
 import os
 import sys
 import json
@@ -26,46 +20,9 @@ CURRENT_VERSION = 'v5.1.0'
 
 class MainApp(tkb.Window):
     def __init__(self):
-        super().__init__('Project AtBS', 'superhero', resizable=(False, False))
-        self.withdraw()
-        self.config(bg=BG_COLOR)
-        self.iconphoto(False, tk.PhotoImage(file='assets/img/atbs_icon.png'))
-
-        self.button_images = []
-        # Create buttons
-        self.cp_btn = self.create_button(
-            'Payment', 'cc_icon.png', self.open_CardPaymentForm)
-        self.rf_btn = self.create_button(
-            'Refill', 'rx.png', self.open_RefillCoordination)
-        self.wud_btn = self.create_button(
-            'Wrap Up', 'cal_calc.png', self.open_WrapUpDateCalculator)
-        self.m_btn = self.create_button(
-            'Map', 'map_icon.png', self.open_MapSearch)
-        self.pm_btn = self.create_button(
-            'Password', 'lock_icon.png', self.open_PasswordManager)
-
-        # Place buttons on grid
-        self.cp_btn.grid(column=0, row=0, sticky='EW')
-        self.rf_btn.grid(column=0, row=1, sticky='EW')
-        self.wud_btn.grid(column=0, row=2, sticky='EW')
-        self.m_btn.grid(column=0, row=3, sticky='EW')
-        self.pm_btn.grid(column=0, row=4, sticky='EW')
-
-        # Bind events
-        self.bind('<Enter>', self.pointerEnter)
-        self.bind('<Leave>', self.pointerLeave)
-
-        # Center window to screen
-        self.update_idletasks()
-        win_width = self.winfo_reqwidth()
-        win_height = self.winfo_reqheight()
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        x = int(screen_width/2 - win_width/2)
-        y = int(screen_height/2 - win_width/2)
-        self.geometry(f"{win_width}x{win_height}+{x}+{y}")
-        self.deiconify()
-
+        super().__init__('Card Payment Form', 'superhero', resizable=(False, False))
+        cardpayment = CardPayment(self, self)
+       
         self.after(ms=86_400_000, func=self._check_for_updates_loop) # every 24 hours
 
     def get_exe_script_path(self) -> str:
@@ -75,54 +32,6 @@ class MainApp(tkb.Window):
         else:
             path = os.path.dirname(os.path.abspath(__file__))
         return path
-
-    def create_button(self, text, image_path, command):
-        img_open = Image.open(fp=f'assets/img/{image_path}')
-        img_resized = img_open.resize(size=(50, 50))
-        self.img = ImageTk.PhotoImage(img_resized)
-        self.button_images.append(self.img)
-        button = tk.Button(text=f'  {text}', image=self.img, compound='left', font=FONT, fg=FG_COLOR,
-                           bg=BG_COLOR, activebackground=ACTIVE_BG_COLOR, command=command,
-                           anchor='w', padx=10, pady=5, width=207, activeforeground=ACTIVE_FG_COLOR, autostyle=False)
-        return button
-
-    def open_WrapUpDateCalculator(self):
-        """Instantiate WrapUpDateCalculator object in a new TopLevel window."""
-        WrapUpDateCalculator()
-
-    def open_CardPaymentForm(self):
-        """Instantiate CardPayment object in a new TopLevel window."""
-        window = tkb.Toplevel('Card Payment Form', 'superhero', resizable=(False, False))
-        CardPayment(window)
-
-    def open_MapSearch(self):
-        """Instantiate Map object in a new TopLevel window."""
-        Map()
-
-    def open_RefillCoordination(self):
-        """Instantiate Refill Coordination object in a new TopLevel window."""
-        RefillTemplate()
-
-    def open_PasswordManager(self):
-        """Instantiate Password Manager in a new TopLevel window."""
-        a = Authenticator()
-        self._check_isVerified_flag(a)
-
-    def _check_isVerified_flag(self, authenticator):
-        """Recursively checks isVerified flag to signal instantiation of Password Manager object."""
-        if not authenticator.isVerified:
-            self.after(50, self._check_isVerified_flag, authenticator)
-        else:
-            authenticator.top.destroy()
-            PasswordManager()
-
-    def pointerEnter(self, event):
-        """Change button color on mouse hover."""
-        event.widget['bg'] = HOVER_BUTTON_COLOR
-
-    def pointerLeave(self, event):
-        """Change button color back to normal when mouse leave button."""
-        event.widget['bg'] = BG_COLOR
 
     def check_for_new_updater_version(self):
         """Check for new version and update the App Update Manager"""
