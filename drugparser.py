3363ad388e534bdaf588be178a65911fea63c9f7
michael-hoang <mike081522@gmail.com>
2023-04-29 22:44:32 -0700

    Create open_website and sign_in methods

--- drugparser.py
+++ drugparser.py
@@ -13,25 +13,30 @@ class DrugParser:
     """
     A class for parsing drung information from AmerisourceBergen's ABC ordering platform.
     """
+
     def __init__(self, driver_path, mode='default'):
-        service = Service(driver_path)
-        options = Options()
+        self.service = Service(driver_path)
+        self.options = Options()
         if mode == 'headless':
-            options.add_argument('--headless')
+            self.options.add_argument('--headless')
         elif mode == 'default':
-            options.add_experimental_option('detach', True)
-        
-        driver = webdriver.Edge(
-            service=service, options=options
+            self.options.add_experimental_option('detach', True)
+
+        self.driver = webdriver.Edge(
+            service=self.service, options=self.options
         )
-        website_url = 'https://abcorderhs.amerisourcebergen.com/'
+        self.website_url = 'https://abcorderhs.amerisourcebergen.com/'
 
+    def open_website(self):
+        self.driver.get(self.website_url)
 
-
-
-
-
-
+    def sign_in(self, username: str, password: str):
+        username_input = self.driver.find_element(By.ID, 'logonuidfield')
+        username_input.send_keys(username)
+        password_input = self.driver.find_element(By.ID, 'logonpassfield')
+        password_input.send_keys(password)
+        sign_in_btn = self.driver.find_element(By.NAME, 'uidPasswordLogon')
+        sign_in_btn.click()
 
 
 if __name__ == '__main__':
@@ -43,8 +48,12 @@ if __name__ == '__main__':
     PASSWORD = os.environ.get('PASSWORD')
 
     webdriver_path = r'C:\Users\Mike\OneDrive\Desktop\edgedriver_win64\msedgedriver.exe'
-
-
+    dp = DrugParser(webdriver_path)
+    dp.open_website()
+    time.sleep(3)
+    dp.sign_in(USER, PASSWORD)
+    time.sleep(4)
+    
 
     # data = pd.read_excel('./assets/data/drugs.xlsx')
     # item_df = pd.DataFrame(data, columns=['Item'])
