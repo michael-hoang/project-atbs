016fe8168edae0758a36af7c0f04231704f9dad9
michael-hoang <mike081522@gmail.com>
2023-04-30 01:41:26 -0700

    Create dropship.py

---
+++ dropship.py
@@ -0,0 +1,9 @@
+import ttkbootstrap as tkb
+
+
+class DropShipLookUp(tkb.Frame):
+    """GUI for searching if a drug is ordered through drop ship."""
+
+    def __init__(self):
+        super.__init__()
+        
